80a9540773ba6afd01c5222b894f45be280b47bd
Cyrus <cyrus@MacBook-Pro-de-Cyrus.local>
2019-02-08 16:20:37 -0500

    Pushing test

---
+++ test.js
@@ -0,0 +1,8 @@
+const myObject = [
+  {cyrus: "son", gender: "male"},
+  {azi: "mom", gender: "female"},
+  {filou: "dad", gender: "neutral"},
+  {louisie: "sister", gender: "female"}
+];
+
+function extractFemale(
