93f1422a15b90149e9b8ddfb01422bc8c779f116
Cyrus <cyrus@MacBook-Pro-de-Cyrus.local>
2019-02-09 10:50:55 -0500

    clearning the repo

--- test.js
+++
@@ -1,8 +0,0 @@
-const myObject = [
-  {cyrus: "son", gender: "male"},
-  {azi: "mom", gender: "female"},
-  {filou: "dad", gender: "neutral"},
-  {louisie: "sister", gender: "female"}
-];
-
-function extractFemale(
